789dbba0929e62547385d9905478d8bb0f23228f
Robert Gauld <robert@robertgauld.co.uk>
2017-06-02 18:06:21 +0100

    Make Fetcher.fetch a class method

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## Version 0.0.2
+
+  * Fetcher.fetch is now a class mehtod
+
 ## Version 0.0.1
 
   * Gemification of a personal utility
--- lib/snmp_table_viewer/fetcher.rb
+++ lib/snmp_table_viewer/fetcher.rb
@@ -3,19 +3,13 @@ module SNMPTableViewer
   # Class to fetch the information from an SNMP host.
   class Fetcher
 
-    # Create a new fetcher.
-    # @param snmp_options [Hash] The options to pass to NETSNMP::Client.new
-    # @return [Fetcher]
-    def initialize(**snmp_options)
-      @snmp_options = snmp_options
-    end
-
     # Fetch the data using SNMP.
     # @param base_oid [String] The OID to start the SNMP walk from
+    # @param **snmp_options [Hash] The options to pass to NETSNMP::Client.new
     # @return [Array<Array<#to_s>>] A two dimensional array containing objects in each cell (at 'address' data[row][col])
-    def fetch(base_oid)
+    def self.fetch(base_oid:, **snmp_options)
       data = Array.new
-      NETSNMP::Client.new(@snmp_options) do |manager|
+      NETSNMP::Client.new(snmp_options) do |manager|
         manager.walk(oid: base_oid).each do |oid, value|
           col, row = oid.split('.')[-2..-1].map{ |i| i.to_i - 1}
           data[row] ||= []
